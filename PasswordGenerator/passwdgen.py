ae8d4fd1990c4648074e4cd210d4c83dcc4d0a9c
Yao Wang <ywang011@odu.edu>
2022-06-12 18:08:59 -0400

    minor fix

--- PasswordGenerator/passwdgen.py
+++ PasswordGenerator/passwdgen.py
@@ -4,7 +4,7 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-print("Welcome to the PyPassword Generator!")
+print("Welcome to the PyPassword Generator v0.2!")
 def passwordLoop():
   nr_letters= int(input("How many letters would you like in your password?\n")) 
   nr_symbols = int(input(f"How many symbols would you like?\n"))
